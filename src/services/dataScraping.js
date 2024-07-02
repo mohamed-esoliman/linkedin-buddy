1088025af13c24793dda1f7c0a05ab5d3bab2b85
Mohamed Soliman <mohamademad8330@gmail.com>
2024-07-01 20:41:40 -0400

    fix profile scraping for images and companies

--- src/services/dataScraping.js
+++ src/services/dataScraping.js
@@ -7,22 +7,24 @@ export const extractProfileData = async (tabId) => {
             {
                 target: {tabId},
                 function: () => {
-                    const profile = {};
                     try {
-                        const nameElement = document.querySelector("#profile-content > div > div > div > div > main > section > div > div > div > div > span > a > h1");
-                        profile.name = nameElement ? nameElement.textContent.trim() : "";
-
-                        const imageElement = document.querySelector("#profile-content > div > div > div > div > main > section > div > div > div > div > button > img");
-                        profile.picture = imageElement ? imageElement.src : "../media/person.png";
-
-                        const bioElement = document.querySelector("#profile-content > div > div > div > div > main > section > div > div > div > div > div");
-                        profile.bio = bioElement ? bioElement.textContent.trim() : "";
-
-                        const experienceSection = document.getElementById("experience").parentElement;
-                        const companyElement = experienceSection.querySelector("div > ul > li:nth-child(1) > div > div > div > div > span > span");
-                        profile.company = companyElement ? companyElement.textContent.trim() : "";
+                        const profile = {};
 
                         profile.url = window.location.href;
+
+                        const nameElement = document.querySelector("#profile-content > div > div > div > div > main > section > div > div > div > div > span > a > h1");
+                        profile.name = nameElement?.textContent.trim() || "";
+
+                        const imageElement = document.querySelector("#profile-content > div > div > div > div > main > section > div > div > div > div > div > div > button > img");
+                        profile.picture = imageElement?.src || "../media/person.png";
+                        
+                        //todo: fix scraping the bio
+                        const bioElement = document.querySelector("#profile-content > div > div > div > div > main > section > div > div > div > div");
+                        profile.bio = bioElement?.textContent.trim() || "";
+
+                        const companyElement = document.getElementById("experience").parentElement.querySelector("div > ul > li:nth-child(1) > div > div > div > a > div > div > div > div > span");
+                        profile.company = companyElement ? companyElement.textContent.trim() : "";
+
                         return profile;
 
                     } catch (error) {
@@ -31,6 +33,7 @@ export const extractProfileData = async (tabId) => {
                     }
                 }
             },
+
             (result) => {
                 if (chrome.runtime.lastError) {
                     reject(chrome.runtime.lastError.message);
